1199f9390ce4f1d56f485219d855f93288f761e8
ajswl <kkhhjj1209@gmail.com>
2024-08-13 16:28:54 +0900

    왜 403이지

--- src/main/java/com/example/global/config/SecurityConfig.java
+++ src/main/java/com/example/global/config/SecurityConfig.java
@@ -2,31 +2,32 @@ package com.example.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.HttpSecurityDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityConfig1(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((val)->
-                        val.requestMatchers("/user/logIn", "/user/sinUp", "/").permitAll()
-                                .anyRequest().authenticated()
-                )
+                        val.anyRequest().permitAll()
+//                        val.requestMatchers("/user/logIn", "/user/sinUp", "/").permitAll()
+//                                .anyRequest().authenticated()
+                );
+        /*
                 .formLogin((arg)-> arg.disable())//혼자 해보기
                 .logout((val)->              //얘도 혼자 해보기
                         val.logoutUrl("/logout")
                                 .logoutSuccessUrl("/")
                 );
                 //.oauth2Login()
+         */
 
         return http.build();
-
     }
 }
+
+
